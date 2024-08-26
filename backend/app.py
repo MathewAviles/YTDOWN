0560319561b9a1513b03e494b19a41efdc3d86d8
MathewAviles <juanaviles12345@gmail.com>
2024-08-26 18:20:18 -0500

    first commit

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS 
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
+
+#helloworld
+@app.route('/', methods=['GET'])
+def hello():
+    return("Hello from Flask!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
