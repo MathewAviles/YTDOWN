3a477734dc69eda2dabc68f311cf2cdf0a75d559
MathewAviles <juanaviles12345@gmail.com>
2024-08-27 17:24:33 -0500

    front implementado con vue 3

--- backend/app.py
+++ backend/app.py
@@ -9,13 +9,13 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 #helloworld
 @app.route('/', methods=['GET'])
 def hello():
-    return("Hello from Flask!")
+    return("Index page")
 
 
 
 @app.route('/downloader', methods=['GET'])
 def downloader():
-    return("Hello from Flask!2")
+    return("Dowloader listo")
 
 if __name__ == '__main__':
     app.run(debug=True)
